2e06bd9f0ce9bc549a5e66addd6886130357ce4b
MuydinovAsadbek <asadbekmoydinov06@gmail.com>
2022-06-22 23:48:49 +0500

    Add site-header modal card

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const elBasketBtn = document.querySelector('.site-header__icon');
+const elModalCard = document.querySelector('.site-header__modal-card-d-none');
+
+if (elBasketBtn) {
+    elBasketBtn.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        elModalCard.classList.toggle('modal-card-d-block')
+    })
+}
+
+
 // COUNTING SPLIT AND PUSH
 const elSplit = document.querySelector('.split-btn');
 const elPush = document.querySelector('.push-btn');
